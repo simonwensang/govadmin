a6811ea10e261f3e73fa5c17d8936a0c98d3bb1f
yiwen.sang桑以文 <sang.yiwen@chinaredstar.com>
2019-10-14 16:26:24 +0800

    验证码

---
+++ src/main/java/com/cpt/model/EventAttachmentExample.java
@@ -0,0 +1,460 @@
+package com.cpt.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventAttachmentExample {
+    protected String orderByClause;
+
+    protected boolean distinct;
+
+    protected List<Criteria> oredCriteria;
+
+    public EventAttachmentExample() {
+        oredCriteria = new ArrayList<Criteria>();
+    }
+
+    public void setOrderByClause(String orderByClause) {
+        this.orderByClause = orderByClause;
+    }
+
+    public String getOrderByClause() {
+        return orderByClause;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public List<Criteria> getOredCriteria() {
+        return oredCriteria;
+    }
+
+    public void or(Criteria criteria) {
+        oredCriteria.add(criteria);
+    }
+
+    public Criteria or() {
+        Criteria criteria = createCriteriaInternal();
+        oredCriteria.add(criteria);
+        return criteria;
+    }
+
+    public Criteria createCriteria() {
+        Criteria criteria = createCriteriaInternal();
+        if (oredCriteria.size() == 0) {
+            oredCriteria.add(criteria);
+        }
+        return criteria;
+    }
+
+    protected Criteria createCriteriaInternal() {
+        Criteria criteria = new Criteria();
+        return criteria;
+    }
+
+    public void clear() {
+        oredCriteria.clear();
+        orderByClause = null;
+        distinct = false;
+    }
+
+    protected abstract static class GeneratedCriteria {
+        protected List<Criterion> criteria;
+
+        protected GeneratedCriteria() {
+            super();
+            criteria = new ArrayList<Criterion>();
+        }
+
+        public boolean isValid() {
+            return criteria.size() > 0;
+        }
+
+        public List<Criterion> getAllCriteria() {
+            return criteria;
+        }
+
+        public List<Criterion> getCriteria() {
+            return criteria;
+        }
+
+        protected void addCriterion(String condition) {
+            if (condition == null) {
+                throw new RuntimeException("Value for condition cannot be null");
+            }
+            criteria.add(new Criterion(condition));
+        }
+
+        protected void addCriterion(String condition, Object value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value));
+        }
+
+        protected void addCriterion(String condition, Object value1, Object value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        public Criteria andIdIsNull() {
+            addCriterion("id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIsNotNull() {
+            addCriterion("id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdEqualTo(Integer value) {
+            addCriterion("id =", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotEqualTo(Integer value) {
+            addCriterion("id <>", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThan(Integer value) {
+            addCriterion("id >", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("id >=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThan(Integer value) {
+            addCriterion("id <", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThanOrEqualTo(Integer value) {
+            addCriterion("id <=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIn(List<Integer> values) {
+            addCriterion("id in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotIn(List<Integer> values) {
+            addCriterion("id not in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdBetween(Integer value1, Integer value2) {
+            addCriterion("id between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("id not between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdIsNull() {
+            addCriterion("event_id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdIsNotNull() {
+            addCriterion("event_id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdEqualTo(Integer value) {
+            addCriterion("event_id =", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdNotEqualTo(Integer value) {
+            addCriterion("event_id <>", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdGreaterThan(Integer value) {
+            addCriterion("event_id >", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("event_id >=", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdLessThan(Integer value) {
+            addCriterion("event_id <", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdLessThanOrEqualTo(Integer value) {
+            addCriterion("event_id <=", value, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdIn(List<Integer> values) {
+            addCriterion("event_id in", values, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdNotIn(List<Integer> values) {
+            addCriterion("event_id not in", values, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdBetween(Integer value1, Integer value2) {
+            addCriterion("event_id between", value1, value2, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andEventIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("event_id not between", value1, value2, "eventId");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentIsNull() {
+            addCriterion("attachment is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentIsNotNull() {
+            addCriterion("attachment is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentEqualTo(String value) {
+            addCriterion("attachment =", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNotEqualTo(String value) {
+            addCriterion("attachment <>", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentGreaterThan(String value) {
+            addCriterion("attachment >", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentGreaterThanOrEqualTo(String value) {
+            addCriterion("attachment >=", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentLessThan(String value) {
+            addCriterion("attachment <", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentLessThanOrEqualTo(String value) {
+            addCriterion("attachment <=", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentLike(String value) {
+            addCriterion("attachment like", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNotLike(String value) {
+            addCriterion("attachment not like", value, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentIn(List<String> values) {
+            addCriterion("attachment in", values, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNotIn(List<String> values) {
+            addCriterion("attachment not in", values, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentBetween(String value1, String value2) {
+            addCriterion("attachment between", value1, value2, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNotBetween(String value1, String value2) {
+            addCriterion("attachment not between", value1, value2, "attachment");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameIsNull() {
+            addCriterion("attachment_name is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameIsNotNull() {
+            addCriterion("attachment_name is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameEqualTo(String value) {
+            addCriterion("attachment_name =", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameNotEqualTo(String value) {
+            addCriterion("attachment_name <>", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameGreaterThan(String value) {
+            addCriterion("attachment_name >", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameGreaterThanOrEqualTo(String value) {
+            addCriterion("attachment_name >=", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameLessThan(String value) {
+            addCriterion("attachment_name <", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameLessThanOrEqualTo(String value) {
+            addCriterion("attachment_name <=", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameLike(String value) {
+            addCriterion("attachment_name like", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameNotLike(String value) {
+            addCriterion("attachment_name not like", value, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameIn(List<String> values) {
+            addCriterion("attachment_name in", values, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameNotIn(List<String> values) {
+            addCriterion("attachment_name not in", values, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameBetween(String value1, String value2) {
+            addCriterion("attachment_name between", value1, value2, "attachmentName");
+            return (Criteria) this;
+        }
+
+        public Criteria andAttachmentNameNotBetween(String value1, String value2) {
+            addCriterion("attachment_name not between", value1, value2, "attachmentName");
+            return (Criteria) this;
+        }
+    }
+
+    public static class Criteria extends GeneratedCriteria {
+
+        protected Criteria() {
+            super();
+        }
+    }
+
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+    }
+}
